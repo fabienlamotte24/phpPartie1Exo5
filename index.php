2e2ee499cc21955f43d98e83968eb5e86719c3ff
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-25 16:09:45 +0200

    exo 5 partie 1 php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $var;
+        settype($var, 'int');
+        echo $var;
+       ?>
+    </p>
+  </body>
+</html>
